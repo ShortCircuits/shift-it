fa7e496c7634480303675c208c967226229e8a0f
a-faraz <anjumfaraz10@gmail.com>
2016-09-20 14:12:21 -0500

    controller tests edited

--- tests/Controllers/controllers.tests.js
+++ tests/Controllers/controllers.tests.js
@@ -1,18 +1,19 @@
 describe('Controllersss', function(){
-    var scope;
+    var scope, controller, modal;
 
     // load the controller's module
     beforeEach(module('starter.controllers'));
 
     beforeEach(inject(function($rootScope, $controller) {
         scope = $rootScope.$new();
-        $controller('AppCtrl', {$scope: scope});
+        modal = jasmine.createSpyObj('modal', ['show', 'hide']);
+        controller = $controller('ProfileCtrl', {$scope: scope});
     }));
 
     // tests start here
-    // it('should be defined', function(){
-
-    //     expect(scope.loginData).toBeUndefined();
-    //     expect(scope.loginData).toEqual(profileData.name);
-    // });
+    //it('should show the modal', function () {
+    //    scope.openEditProfile();
+    //    expect(modal.show).toHaveBeenCalled();
+    //});
 });
+
